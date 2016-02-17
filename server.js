f94426665febeec4c601225ea173bb52a5b59ee9
Foxeyes Only <foxeyesonly@gmail.com>
2016-02-17 13:37:06 -0600

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var portNumber = 3000;
+
+var middleware = {
+	requireAuthentication: function(request, response, next){
+		console.log('private meh!');
+		next();
+	},
+	logger: function(request, response, next){
+		console.log('Request[' + new Date().toString() + ']' + request.method + ' ' + request.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(request, response){
+	response.send('About meh!');
+});
+
+app.use(express.static(__dirname + '/public'));
+//console.log(__dirname);
+
+app.listen(portNumber, function(){
+	console.log('Server started on port ' + portNumber + '.');
+});
